f09d656d0a6caac1fa09130358e5690d8907e258
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-14 16:52:32 +0530

    Update HttpService.js

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,13 +1,13 @@
-const API_KEY = 'at_VDOQ35GZC6fveKlNEYsux8s4riz9C'; //... Set your api key so that the app works
+// const API_KEY = 'at_VDOQ35GZC6fveKlNEYsux8s4riz9C'; //... Set your api key so that the app works
 
-export const getCurrentIp = async () => {
-	const response = await fetch('https://api.ipify.org');
-	return await response.text();
-};
+// export const getCurrentIp = async () => {
+// 	const response = await fetch('https://api.ipify.org');
+// 	return await response.text();
+// };
 
-export const getIp = async ip => {
-	const response = await fetch(
-		`https://geo.ipify.org/api/v2/country,city?apiKey=at_VDOQ35GZC6fveKlNEYsux8s4riz9C&ipAddress=8.8.8.8`
-	);
-	return await response.json();
-};
+// export const getIp = async ip => {
+// 	const response = await fetch(
+// 		`https://geo.ipify.org/api/v2/country,city?apiKey=at_VDOQ35GZC6fveKlNEYsux8s4riz9C&ipAddress=8.8.8.8`
+// 	);
+// 	return await response.json();
+// };
