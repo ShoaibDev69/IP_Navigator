530b87d71048bfc3131a1f2a0b8922844ac6d9a2
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-10-14 17:00:35 +0530

    Update HttpService.js

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -12,37 +12,3 @@
 // 	return await response.json();
 // };
 // Set API key as environment variable
-const API_KEY = `at_VDOQ35GZC6fveKlNEYsux8s4riz9C`;
-
-export const getCurrentIp = async () => {
-  try {
-    const response = await fetch('https://api.ipify.org?format=json');
-
-    if(!response.ok) {
-      throw new Error('Failed to get current IP'); 
-    }
-
-    return await response.json(); 
-  } catch(error) {
-    console.error(error);
-    return null;
-  }
-}
-
-export const getIpDetails = async (ip) => {
-  try {
-    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
-
-    const response = await fetch(url);
-
-    if(!response.ok) {
-      throw new Error('Failed to get IP details');
-    }
-
-    return await response.json();
-
-  } catch(error) {
-    console.error(error);
-    return null; 
-  }
-}
