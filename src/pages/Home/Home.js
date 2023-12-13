c861c7280b5f69704760157653b4f9bcfa0d0bfa
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-13 19:14:28 +0530

    Initial commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 
-//* Components
 import Header from '../../components/Header/Header';
 import Main from '../../components/Main/Main';
 
 function Home() {
-	const [config, setConfig] = React.useState(null);
+	const [config, setConfig] = useState(null);
 
 	return (
 		<>
