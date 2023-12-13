c41f67f2c4ca9f12e47b6aa7d5d4b6fad5625215
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-13 19:08:58 +0530

    Initial commit

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,13 +1,29 @@
 const API_KEY = 'at_VDOQ35GZC6fveKlNEYsux8s4riz9C'; //... Set your api key so that the app works
 
 export const getCurrentIp = async () => {
-	const response = await fetch('https://api.ipify.org');
-	return await response.text();
+	try {
+		const response = await fetch('https://api.ipify.org');
+		if (!response.ok) {
+			throw new Error('Failed to fetch current IP');
+		}
+		return response.text();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
 };
 
 export const getIp = async ip => {
-	const response = await fetch(
-		`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip};`
-	);
-	return await response.json();
+	try {
+		const response = await fetch(
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip};`
+		);
+		if (!response.ok) {
+			throw new Error('Failed to fetch IP information');
+		}
+		return response.json();
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
 };
