0bb447f29b8c80a10d395b4902f66a2d1bd0642b
ShoaibDev69 <shoaib.developer69@gmail.com>
2023-12-13 19:11:14 +0530

    Initial commit

--- src/services/SchemeService.js
+++ src/services/SchemeService.js
@@ -1,3 +1,4 @@
+import { window, matchMedia } from 'some-package'; // Replace 'some-package' with the actual package name
 export const preferDarkScheme =
 	window.matchMedia &&
 	window.matchMedia('(prefers-color-scheme: dark)').matches;
